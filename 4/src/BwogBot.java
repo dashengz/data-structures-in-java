a6446b6001bca2485d698027b9aa051972b56942
dashengz <contact@dashengzhang.me>
2015-11-12 02:09:28 -0500

    Add readFile()

--- 4/src/BwogBot.java
+++ 4/src/BwogBot.java
@@ -1,13 +1,26 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class BwogBot {
 
-  public BwogBot() {
+  SeparateChainingMap<String, Integer> map;
 
+  public BwogBot() {
+    map = new SeparateChainingMap<>();
   }
 
   public void readFile(String fileName) throws IOException {
+    File file = new File(fileName);
+    Scanner scanner = new Scanner(file);
+    while (scanner.hasNext()) {
+      String word = scanner.next();
+      if (map.get(word) == null)
+        map.put(word, 1);
+      else
+        map.put(word, map.get(word) + 1);
+    }
 
   }
 
@@ -25,8 +38,9 @@ public class BwogBot {
 
   public static void main(String[] args) throws IOException {
     BwogBot bot = new BwogBot();
-    bot.readFile("comments.txt");
-    System.out.println(bot.getCount("hamdel")); // because linan's hungry now
-    System.out.println(bot.getNMostPopularWords(100));
+    bot.readFile("text.txt");
+//    System.out.println(bot.getCount("true")); // because linan's hungry now
+//    System.out.println(bot.getNMostPopularWords(100));
+    System.out.println(bot.map.get("to"));
   }
 }
