88f6a4d65d2347987fa789e00d48ab2538365ad9
dashengz <contact@dashengzhang.me>
2015-11-12 14:54:00 -0500

    Optimize bwogbot

--- 4/src/BwogBot.java
+++ 4/src/BwogBot.java
@@ -6,7 +6,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BwogBot {
-
+  /*
+  * Rationale of using SeparateChainingMap instead of AVLMap:
+  * The AVLMap uses an AVL Tree, whose put() and get() methods run in O(Log(N))
+  * while those of a SeparateChainingMap hash table
+  * run in O(1) (with a small load factor since it has upsize() that rehashes the table)
+  * In this problem we have a huge data set that a lot of put() and get() calls are required
+  * So using the SCM is a better choice.
+  */
   SeparateChainingMap<String, Integer> map;
 
   public BwogBot() {
@@ -40,16 +47,27 @@ public class BwogBot {
       }
     }
 
-    words.sort(new Comparator<Pair<String, Integer>>() {
-      @Override
-      public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
-        if (o1.value.equals(o2.value))
-          return 0;
-        if (o1.value.compareTo(o2.value) < 0)
-          return 1;
-        else return -1;
-      }
-    });
+//    words.sort(new Comparator<Pair<String, Integer>>() {
+//      @Override
+//      public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
+//        if (o1.value.equals(o2.value))
+//          return 0;
+//        if (o1.value.compareTo(o2.value) < 0)
+//          return 1;
+//        else return -1;
+//      }
+//    });
+
+    // use lambda expressions
+    words.sort(
+            (o1, o2) -> {
+              if (o1.value.equals(o2.value))
+                return 0;
+              if (o1.value.compareTo(o2.value) < 0)
+                return 1;
+              else return -1;
+            }
+    );
 
     for (int i = 0; i < n; i++) {
       list.add(words.get(i).key);
@@ -67,6 +85,20 @@ public class BwogBot {
     bot.readFile("comments.txt");
     System.out.println(bot.getCount("hamdel")); // because linan's hungry now
     System.out.println(bot.getNMostPopularWords(100));
-//    System.out.println(bot.getCount("to"));
+
+    /*
+    * Top 100 Popular Words:
+    * hodor, the, to, a, is, i, of, you, and, that, this, it, in, s, nt, for, are, not, on, do, was, have, they, be, with,
+    * so, what, about, all, at, we, as, if, like, people, your, columbia, who, just, bwog, my, no, but, there, an, how, one,
+    * or, can, get, from, re, why, has, m, me, does, their, out, by, think, he, she, know, would, students, did, because,
+    * were, should, rape, really, more, up, when, these, will, go, campus, some, been, good, even, time, also, her, actually,
+    * being, than, only, here, school, going, any, someone, barnard, them, student, year, way
+    */
+
+    /*
+    * From the list we can't really see a distinct pattern of the bwog language style:
+    * The top of the list, hodor, comes from a single spam comment.
+    * Most of the words are a. / pron. / adv. etc. instead of nouns or adjectives which have concrete meanings and personal opinions.
+    * Maybe a way to handle this is to filter the list by setting a limit for each word (more than three letters) */
   }
 }
