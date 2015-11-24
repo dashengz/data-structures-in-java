ee9faba058b62d672df808d0b58eb01a66e9465a
dashengz <contact@dashengzhang.me>
2015-11-23 20:48:35 -0500

    Problem 4 2nd attempt

--- 5/src/KBestCounter.java
+++ 5/src/KBestCounter.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.List;
@@ -6,9 +7,10 @@ public class KBestCounter<T extends Comparable<T>> {
    
     PriorityQueue<T> heap;
     private int kBest;
+    private final int INITIAL_SIZE = 0;
 
     public KBestCounter(int k) {
-        heap = new PriorityQueue<>();
+        heap = new PriorityQueue<>(INITIAL_SIZE, Collections.reverseOrder());
         kBest = k;
     }
 
@@ -19,7 +21,9 @@ public class KBestCounter<T extends Comparable<T>> {
     public List<T> kbest() {
         LinkedList<T> kBestList = new LinkedList<>();
         for (int i = 0; i < kBest; i++) {
-            kBestList.add(heap.poll());
+            while (!heap.isEmpty()) {
+                kBestList.add(heap.poll());
+            }
         }
         return kBestList;
     }
