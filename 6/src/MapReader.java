fbbb37d96ba2cdf01bd897c76cb2597da3f12a43
dashengz <contact@dashengzhang.me>
2015-12-04 14:07:25 -0500

    Problem 5 finished

--- 6/src/MapReader.java
+++ 6/src/MapReader.java
@@ -1,11 +1,48 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class MapReader {
 
   public static Graph readGraph(String vertexfile, String edgefile) {
-    return null; // TODO 
+    Graph mapGraph = new Graph();
+    // try and catch blocks to catch file read exceptions
+    try {
+      File vFile = new File(vertexfile);
+      File eFile = new File(edgefile);
+      // use scanners to read files
+      Scanner vScanner = new Scanner(vFile);
+      Scanner eScanner = new Scanner(eFile);
+      while (vScanner.hasNext()) {
+        String vLine = vScanner.nextLine();
+        // use String.split() to split commas
+        String[] vSplit = vLine.split(",");
+        // add vertices to graph
+        mapGraph.addVertex(new Vertex(vSplit[0],Integer.parseInt(vSplit[1]),Integer.parseInt(vSplit[2])));
+      }
+      while (eScanner.hasNext()) {
+        String eLine = eScanner.nextLine();
+        String[] eSplit = eLine.split(",");
+        // add undirected edges to graph
+        mapGraph.addUndirectedEdge(eSplit[0],eSplit[1],1.0);
+      }
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+
+    return mapGraph;
   }
 
   public static void main(String[] args) {
-    System.out.println("No operation."); // TODO 
-  }
+    // get the args passed in when the program is called using command line
+    // $ java MapReader ttrvertices.txt ttredges.txt
+    String vertexfile = args[0];
+    String edgefile = args[1];
 
+    // based on the file names input, create a map graph
+    Graph mapGraph = readGraph(vertexfile, edgefile);
+
+    // display the map graph
+    DisplayGraph display = new DisplayGraph(mapGraph);
+    display.setVisible(true);
+  }
 }
