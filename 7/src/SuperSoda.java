542e9c5d362458857c3765b10a8b645cd8046c2c
dashengz <contact@dashengzhang.me>
2015-12-11 18:06:50 -0500

    Test Max Soda again

--- 7/src/SuperSoda.java
+++ 7/src/SuperSoda.java
@@ -89,7 +89,7 @@ public class SuperSoda {
     int[] sodaSizes = new int[] { 1, 6, 12, 25, 36 };
     double[] costs = new double[] { 0.8, 4, 7.5, 14, 20 };
     System.out.println(minimalSodaCost(sodaSizes, costs, 100));
-    System.out.println(maximumSodaNumber(sodaSizes, costs, 56.0));
+    System.out.println(maximumSodaNumber(sodaSizes, costs, 52.8));
     System.out.println(Arrays.toString(minimalSodaCostCombinations(sodaSizes, costs, 105)));
   }
 }
