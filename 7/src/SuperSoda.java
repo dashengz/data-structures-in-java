546961b42c7a4f8946ab1aa96b8882d4f6970f99
dashengz <contact@dashengzhang.me>
2015-12-11 19:11:13 -0500

    Bug fix / Partial Min Soda Combo

--- 7/src/SuperSoda.java
+++ 7/src/SuperSoda.java
@@ -12,7 +12,7 @@ public class SuperSoda {
     minCosts[0] = 0;
     // minCosts[1,6,12,25,36]
     for (int i = 0; i < sodaSizes.length; i++) {
-      minCosts[sodaSizes[i]] = costs[i];
+      if (n >= sodaSizes[i]) minCosts[sodaSizes[i]] = costs[i];
     }
 
     // For debug
@@ -55,7 +55,7 @@ public class SuperSoda {
     double[] minCosts = new double[guess+1];
     minCosts[0] = 0;
     for (int i = 0; i < sodaSizes.length; i++) {
-      minCosts[sodaSizes[i]] = costs[i];
+      if (guess >= sodaSizes[i]) minCosts[sodaSizes[i]] = costs[i];
     }
     for (int i = 2; i <= guess; i++) {
       LinkedList<Double> tmp = new LinkedList<>();
@@ -79,15 +79,41 @@ public class SuperSoda {
   }
 
   public static int[] minimalSodaCostCombinations(int[] sodaSizes, double[] costs, int n) {
+    // caching table
+    int[][] sodaTable = new int[n+1][sodaSizes.length];
+    // initiate return int array
+    int[] minCostCombo = new int[sodaSizes.length];
 
-    return null; 
+    // base case 0
+    for (int i = 0; i < sodaSizes.length; i++) sodaTable[0][i] = 0;
+    // 1, 6, 12, 25, 36
+    for (int i = 0; i < sodaSizes.length; i++) {
+      if (n >= sodaSizes[i]) {
+        for (int j = 0; j < sodaSizes.length; j++) sodaTable[sodaSizes[i]][j] = 0;
+        sodaTable[sodaSizes[i]][i] = 1;
+      }
+    }
+
+    // For debug
+//    System.out.println(Arrays.toString(sodaTable[0]));
+//    System.out.println(Arrays.toString(sodaTable[1]));
+//    System.out.println(Arrays.toString(sodaTable[6]));
+//    System.out.println(Arrays.toString(sodaTable[12]));
+//    System.out.println(Arrays.toString(sodaTable[25]));
+//    System.out.println(Arrays.toString(sodaTable[36]));
+
+//    for (int i = 2; i <= n; i++) {
+//
+//    }
+
+    return minCostCombo;
   }
 
   public static void main(String[] args) {
     int[] sodaSizes = new int[] { 1, 6, 12, 25, 36 };
     double[] costs = new double[] { 0.8, 4, 7.5, 14, 20 };
-    System.out.println(minimalSodaCost(sodaSizes, costs, 100));
-    System.out.println(maximumSodaNumber(sodaSizes, costs, 52.8));
+    System.out.println(minimalSodaCost(sodaSizes, costs, 17));
+    System.out.println(maximumSodaNumber(sodaSizes, costs, 2.0));
     System.out.println(Arrays.toString(minimalSodaCostCombinations(sodaSizes, costs, 105)));
   }
 }
